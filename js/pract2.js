7efb092d2748d0df8d586699b7f02d97d6d9e5d4
krls99 <79696693+krls99@users.noreply.github.com>
2022-10-23 22:10:15 +0200

    Update pract2.js

--- js/pract2.js
+++ js/pract2.js
@@ -7,7 +7,7 @@ function init() {
     document.body.appendChild(renderer.domElement);
     scene = new THREE.Scene()
     camera = new THREE.PerspectiveCamera(75,window.innerWidth/window.innerHeight,0.1,1000);
-    camera.position.set(90, 200, 350);
+    camera.position.set(-10, 200, 350);
     camera.lookAt(new THREE.Vector3(0, 100, 0));
 }
 
