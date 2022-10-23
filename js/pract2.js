6769c64710c1c522697be6ffb86aa25bbc23d047
krls99 <79696693+krls99@users.noreply.github.com>
2022-10-23 20:14:51 +0200

    Update pract2.js

--- js/pract2.js
+++ js/pract2.js
@@ -1,67 +1,67 @@
-var renderer, scene, camera;
 
-function init()
-{
+let renderer, scene, camera;
+
+function init() {
+
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.setClearColor( new THREE.Color(0x0000AA), 1.0 );
-    document.body.appendChild(renderer.domElement);
+    renderer.setClearColor( new THREE.Color(0xFFFFFF), 1.0 );
+
+    document.body.appendChild( renderer.domElement );
 
     scene = new THREE.Scene();
 
-    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1,1000);
-    camera.position.set(200, 300, 150); #pos cam
-    camera.lookAt( new THREE.Vector3(0,0,0) );  
+    let aspectRatio = window.innerWidth / window.innerHeight;
+    camera = new THREE.PerspectiveCamera( 75, aspectRatio, 0.1, 10000 );
+    camera.position.set( -10, 200, 250 );
+    camera.lookAt( 0, 100, 0 );
+
 }
 
+function loadRobot() {
 
-function loadScene() {
+    let material = new THREE.MeshBasicMaterial( {color:"red", wireframe: true} );
 
-renderer = new THREE.WebGLRenderer();
+    let suelo = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 10, 10 ), material );
+    suelo.position.set(0, 0, 0);
+    suelo.rotation.x = -Math.PI/2;
+    suelo.rotation.z = Math.PI/4;
 
-    var material = new THREE.MeshBasicMaterial( {color:"red", wireframe: true} );
+    let baseCilindro = new THREE.Mesh( new THREE.CylinderGeometry( 50, 50, 15, 40 ), material );
 
-    var suelo = new THREE.Mesh( new THREE.PlaneGeometry( 1000, 1000, 20, 20 ), material );
-    //suelo.position.set(0, 0, 0);
-    //suelo.rotation.x = -Math.PI/2;
-    //suelo.rotation.z = Math.PI/4;
-    var robot = new THREE.Object3D();
-    
-    var baseC = new THREE.Mesh( new THREE.CylinderGeometry( 50, 50, 15, 40 ), material );
-
-    var brazoEsfera = new THREE.Mesh( new THREE.SphereGeometry( 20, 20, 20 ), material );
+    let brazoEsfera = new THREE.Mesh( new THREE.SphereGeometry( 20, 20, 20 ), material );
     brazoEsfera.position.set(0, 120, 0);
 
-    var brazoCaja = new THREE.Mesh( new THREE.BoxGeometry( 18, 120, 12 ), material );
+    let brazoCaja = new THREE.Mesh( new THREE.BoxGeometry( 18, 120, 12 ), material );
     brazoCaja.position.set(0, 60, 0);
 
-    var brazoCilindro = new THREE.Mesh( new THREE.CylinderGeometry( 20, 20, 18, 40 ), material );
+    let brazoCilindro = new THREE.Mesh( new THREE.CylinderGeometry( 20, 20, 18, 40 ), material );
     brazoCilindro.rotation.x = Math.PI/2;
     brazoCilindro.rotation.z = Math.PI/5;
 
-    var antebrazoCilindro = new THREE.Mesh( new THREE.CylinderGeometry( 15, 15, 40, 40 ), material );
+    let antebrazoCilindro = new THREE.Mesh( new THREE.CylinderGeometry( 15, 15, 40, 40 ), material );
     antebrazoCilindro.position.set(0, 200, 0);
     antebrazoCilindro.rotation.x = Math.PI/2;
     antebrazoCilindro.rotation.z = Math.PI/5;
 
-    var antebrazoNervioGeometria = new THREE.BoxGeometry( 4, 80, 4 );
+    let antebrazoNervioGeometria = new THREE.BoxGeometry( 4, 80, 4 );
 
-    var antebrazoNervio1 = new THREE.Mesh( antebrazoNervioGeometria, material );
+    let antebrazoNervio1 = new THREE.Mesh( antebrazoNervioGeometria, material );
     antebrazoNervio1.position.set(4, 160, 4);
 
-    var antebrazoNervio2 = new THREE.Mesh( antebrazoNervioGeometria, material );
+    let antebrazoNervio2 = new THREE.Mesh( antebrazoNervioGeometria, material );
     antebrazoNervio2.position.set(4, 160, -4);
 
-    var antebrazoNervio3 = new THREE.Mesh( antebrazoNervioGeometria, material );
+    let antebrazoNervio3 = new THREE.Mesh( antebrazoNervioGeometria, material );
     antebrazoNervio3.position.set(-4, 160, 4);
 
-    var antebrazoNervio4 = new THREE.Mesh( antebrazoNervioGeometria, material );
+    let antebrazoNervio4 = new THREE.Mesh( antebrazoNervioGeometria, material );
     antebrazoNervio4.position.set(-4, 160, -4);
 
-    var antebrazoBase = new THREE.Mesh( new THREE.CylinderGeometry( 22, 22, 6, 40 ), material );
+    let antebrazoBase = new THREE.Mesh( new THREE.CylinderGeometry( 22, 22, 6, 40 ), material );
     antebrazoBase.position.set(0, 120, 0);
 
-    var pinzas_coordenadas = [
+    let pinzas_coordenadas = [
         0, 0, 0,
         0, 0, 4,
         0, 20, 0,
@@ -76,7 +76,7 @@ renderer = new THREE.WebGLRenderer();
         38, 17, 2
     ];
 
-    var pinzas_indices = [
+    let pinzas_indices = [
         0,2,1, 0,3,2,
         0,2,6, 0,6,4,
         0,1,4, 0,5,4,
@@ -89,7 +89,7 @@ renderer = new THREE.WebGLRenderer();
         8,4,6, 8,6,10
     ]
 
-    var pinza1_geometry = new THREE.Geometry();
+    let pinza1_geometry = new THREE.Geometry();
 
     for (let i = 0; i < pinzas_coordenadas.length; i += 3) {
         let vertice = new THREE.Vector3( pinzas_coordenadas[i], pinzas_coordenadas[i+1], pinzas_coordenadas[i+2] );
@@ -101,23 +101,23 @@ renderer = new THREE.WebGLRenderer();
         pinza1_geometry.faces.push(triangulo);
     }
 
-    var pinza1 = new THREE.Mesh( pinza1_geometry, material );
+    let pinza1 = new THREE.Mesh( pinza1_geometry, material );
     pinza1.position.set( 0, 190, 20 );
     pinza1.rotation.y = -Math.PI/4;
 
-    var pinza2 = new THREE.Mesh( pinza1_geometry, material );
+    let pinza2 = new THREE.Mesh( pinza1_geometry, material );
     pinza2.position.set( 20, 190, -10 );
     pinza2.rotation.y = -Math.PI/4;
 
-    var robot = new THREE.Object3D();
-    var base = new THREE.Object3D();
-    var brazo = new THREE.Object3D();
-    var antebrazo = new THREE.Object3D();
-    var mano = new THREE.Object3D();
+    let robot = new THREE.Object3D();
+    let base = new THREE.Object3D();
+    let brazo = new THREE.Object3D();
+    let antebrazo = new THREE.Object3D();
+    let mano = new THREE.Object3D();
 
     robot.add(base);
 
-    base.add(baseC);
+    base.add(baseCilindro);
     base.add(brazo);
 
     brazo.add(brazoEsfera);
@@ -138,17 +138,32 @@ renderer = new THREE.WebGLRenderer();
 
     robot.position.set( 0, 0, 0 );
 
+    //const axesHelper = new THREE.AxesHelper( 500 );
+    //scene.add( axesHelper );
+
+    //const controls = new THREE.OrbitControls( camera, renderer.domElement );
+    //controls.target.copy(robot.position);
+    //controls.update();
+
     scene.add(suelo);
     scene.add(robot);
-    
+
 }
 
+let antes = Date.now();
+let angulo = 0;
+
+function update() {
+}
 
 function render() {
-    requestAnimationFrame(render);
-    renderer.render(scene,camera);
+
+    requestAnimationFrame( render );
+    update();
+    renderer.render( scene, camera );
+
 }
 
 init();
-loadScene();
+loadRobot();
 render();
