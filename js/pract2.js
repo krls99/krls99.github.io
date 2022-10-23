6861abe98c07ed2e07974bb868eb3a5f39e972c1
krls99 <79696693+krls99@users.noreply.github.com>
2022-10-23 22:39:20 +0200

    Update pract2.js

--- js/pract2.js
+++ js/pract2.js
@@ -96,10 +96,10 @@ function loadScene() {
     pinza.setAttribute('position', new THREE.Float32BufferAttribute(vertex,3));
 
     var pinzaIz = new THREE.Mesh(pinza, material);
-    pinzaIz.rotation.y = -Math.PI / 2;  
+    pinzaIz.rotation.z = -Math.PI / 2;  
 
     var pinzaDe = new THREE.Mesh(pinza, material);
-    pinzaDe.rotation.y = - Math.PI / 4;
+    pinzaDe.rotation.x = - Math.PI / 4;
     pinzaDe.position.set(0, 120, 0);
 
     //MANO
